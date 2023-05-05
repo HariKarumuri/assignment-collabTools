2077bf803d33b87233e8b86ead1677a55b4faae6
hari <harikarumuri1086@gmail.com>
2023-05-05 18:42:58 +0530

    changed the bug of duplicate messages.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
+const requestIp = require('request-ip');
+
 app.use(cors());
+app.use(requestIp.mw());
 
 const server = http.createServer(app);
 
@@ -14,8 +17,13 @@ const io = new Server(server, {
   },
 });
 
+// Initialize an empty list to store banned IP addresses
+const bannedIPs = [];
+
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
+  const ipAddress = socket.handshake.headers['x-real-ip'] || socket.handshake.address; // get the client's IP address
+  console.log(`New client connected with IP address: ${ipAddress}`);
 
   socket.on("join_room", (data) => {
     socket.join(data);
@@ -23,6 +31,21 @@ io.on("connection", (socket) => {
   });
 
   socket.on("send_message", (data) => {
+    // Check if the message contains a hate word
+    const hateWords = ["hate", "racism", "discrimination"]; // Example list of hate words
+    const containsHateWord = hateWords.some((word) => data.message.includes(word));
+
+    if (containsHateWord) {
+      // If the message contains a hate word, ban the user by their IP address
+      if (!bannedIPs.includes(ipAddress)) {
+        bannedIPs.push(ipAddress);
+        console.log(`User with IP address ${ipAddress} has been banned`);
+        socket.emit("hate_word_detected", "Hate word detected. You have been banned.");
+      }
+      return; // Don't send the message to other users
+    }
+
+    // If the message doesn't contain a hate word, send it to other users
     socket.to(data.room).emit("receive_message", data);
   });
 
